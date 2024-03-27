4e8bf9e4bef1d9a837fc99bca85501796eca7a25
JBC <hello@jeanbaptistecalzia.fr>
2024-03-27 16:29:40 +0100

    Create city country tagline price content

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,41 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    const pCityCountry = document.createElement("p");
+    pCityCountry.textContent = `${city}, ${country}`;
+
+    const pTagline = document.createElement("p");
+    pTagline.textContent = tagline;
+
+    const spanPrice = document.createElement("span");
+    spanPrice.textContent = `${price}€/jour`;
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(pCityCountry);
+    article.appendChild(pTagline);
+    article.appendChild(spanPrice);
+    return article;
+  }
+  return {
+    name,
+    picture,
+    portrait,
+    city,
+    country,
+    tagline,
+    price,
+    getUserCardDOM,
+  };
 }
