5953a87b1e71524cbe6497a369a0b31a75e78b7c
JBC <hello@jeanbaptistecalzia.fr>
2024-03-28 14:06:54 +0100

    Create Class Media

---
+++ scripts/models/Media.js
@@ -0,0 +1,10 @@
+class Media {
+  constructor(data) {
+    this._id = data.id;
+    this._photographerId = data.photographerId;
+    this._title = data.title;
+    this._likes = data.likes;
+    this._date = data.date;
+    this._price = data.price;
+  }
+}
