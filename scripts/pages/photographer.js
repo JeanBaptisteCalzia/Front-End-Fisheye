883f258801b12b9c7bc40c092c8f1142bbe9e806
JBC <hello@jeanbaptistecalzia.fr>
2024-04-18 10:01:40 +0200

    Remove useless eventListener

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,20 +2,13 @@ import { getPhotographers, getMedia } from "../api/api.js";
 import { photographerTemplate } from "../templates/photographer.js";
 
 // Retrieve URL data and display index page if photographer page has no id
-window.addEventListener(
-  "load",
-  () => {
-    let searchParams = new URLSearchParams(window.location.search);
-
-    if (searchParams.has("id")) {
-      let galId = searchParams.get("id");
-      console.log(galId);
-    } else {
-      window.location.pathname = "index.html";
-    }
-  },
-  false
-);
+let searchParams = new URLSearchParams(window.location.search);
+if (searchParams.has("id")) {
+  let galId = searchParams.get("id");
+  console.log(galId);
+} else {
+  window.location.pathname = "index.html";
+}
 
 // Retrieve Photographer ID
 const photographerId = new URLSearchParams(window.location.search).get("id");
